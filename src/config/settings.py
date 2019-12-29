a1879fb182af5742c5d666fa08793c09cb9410d4
Denis Costa <deniscostadsc@gmail.com>
2019-12-29 13:27:33 -0300

    Sort imports

--- src/config/settings.py
+++ src/config/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-from decouple import config
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
